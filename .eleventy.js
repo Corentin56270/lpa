49920e72ccf1cdc75be6e9af0ff5887923f93cbd
Corentin56270 <tanguycorentin56@gmail.com>
2025-11-16 19:33:13 +0100

    .eleventy.js tu vas être inclus dans mon commit cette fois ?

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/styles");
     eleventyConfig.addPassthroughCopy("./src/media");
+    eleventyConfig.addPassthroughCopy("./scripts");
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
