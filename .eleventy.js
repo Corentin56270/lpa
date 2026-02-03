bb82d9cf1de212946d0bcfcc91cf81a80e22a67a
Corentin Tanguy <corentin.tanguy2@etu.univ-nantes.fr>
2026-02-03 23:58:52 +0100

    feedback changes (mostly untested)

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
 const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
+    const md = new markdownIt({
+        html: true
+    });
 
     eleventyConfig.addPassthroughCopy("./src/styles");
     eleventyConfig.addPassthroughCopy("./src/media");
     eleventyConfig.addPassthroughCopy("./scripts");
+    eleventyConfig.addPassthroughCopy("./src/admin");
+
+    eleventyConfig.addFilter("markdown", (content) => {
+        return md.render(content);
+    });
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
